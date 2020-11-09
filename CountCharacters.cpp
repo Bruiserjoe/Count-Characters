e06e30485cc1847024f02158357ff8f384af810a
Joe <bruiserjoe01@gmail.com>
2020-11-09 11:40:53 -0600

    Fixed spaces breaking the program
    Cleaned up the code a bit

--- CountCharacters.cpp
+++ CountCharacters.cpp
@@ -1,29 +1,26 @@
-// CountCharacters.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <string>
-#include <stdio.h>
 
 int main()
 {
 	int size = 0;
-	int sizei;
-	char input [100];
+	std::string input;
 	char input2;
 	std::cout << "Enter the string:" << std::endl;
-	std::cin >> input;
-	sizei = strlen(input);
-	std::cout << "Enter char you want to find amount:" << std::endl;
+	std::getline(std::cin, input);
+	std::cout << "Enter char you want to find amount of:" << std::endl;
 	std::cin >> input2;
-	for (int i = 0; i <= sizei; i++) {
-		if (input[i] == input2) {
-			size++;
-			
-			
-		}
+	for (int i = 0; i <= input.size(); i++) {
+			if (input[i] == input2) {
+				size++;
+
+
+			}
 		
 		
 	}
 	std::cout << "Number of characters in string: " << size << std::endl;
+	system("pause");
+
+	return 0;
 }
